17b59f1c5251923a1b99342da9e0a215ccd17aac
Carlos <c.c.oviedo@hotmail.com>
2020-01-29 15:22:03 +0100

    Login server funcionallity

--- app-front/src/componentes/App.js
+++ app-front/src/componentes/App.js
@@ -1,25 +1,36 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import logo from '../logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+
+          <nav>
+            <Link to="/">Login</Link> - 
+            <Link to="/registro"> Registro</Link>
+          </nav>
+          {/* <Route path="/" exact component={ ListarUsuario }/>
+          <Route path="/registro" component={ CrearUsuario }/>
+          <Route path="/api/usuarios/edit/:id" component={ EditarUsuario }/> */}
+        </header>
+      </div>
+    </Router>
   );
 }
 
