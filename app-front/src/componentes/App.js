b9290b22e91178d7176a025dac7b1c9fc935ad01
Arturo.l <arturolanz.cre@gmail.com>
2020-01-29 17:08:09 +0100

    commit registro funcional

--- app-front/src/componentes/App.js
+++ app-front/src/componentes/App.js
@@ -13,11 +13,11 @@ function App(){
     <Router>
     <div className='wrapper'>
     <header className='cabeceraApp col-xs-12'>
-    <nav class="navegadorN1">
-        <div class="iniciarSesion">
+    <nav className="navegadorN1">
+        <div className="iniciarSesion">
         <Link to="/iniciarsesion" className='navLogin'> Iniciar sesión </Link>         </div>
         
-        <div class="registro">
+        <div className="registro">
         <Link to="/registro" className='navRegistro'> Regístrate </Link>
         </div>
     </nav>
--- app-front/src/componentes/RegistroUsuarios.js
+++ app-front/src/componentes/RegistroUsuarios.js
@@ -4,46 +4,118 @@ import './RegistroUsuarios.css'
 
 class RegistroUsuarios extends /*React.*/ Component{
 
-render(){
+    constructor(props){
+    super(props);
+    this.state={
+        nombre: '',
+        edad: '',
+        email: '',
+        password:'',
+    }
+    
+    this.onChangeNombre = this.onChangeNombre.bind(this);
+    this.onChangeApellido = this.onChangeApellido.bind(this);
+    this.onChangeEmail = this.onChangeEmail.bind(this);
+    this.onChangePassword = this.onChangePassword.bind(this);
+    this.onChangeEdad = this.onChangeEdad.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);    
+
+ 
+    }
+
+    onChangeNombre(evt){
+
+        this.setState({// como parámetro recibimos un objeto con la prop email y le asignamos el valor
+            nombre: evt.target.value
+        });
+    }
+    onChangeApellido(evt){
+
+        this.setState({// como parámetro recibimos un objeto con la prop email y le asignamos el valor
+            apellido: evt.target.value
+        });
+    }
+    onChangeEmail(evt){
+
+        this.setState({// como parámetro recibimos un objeto con la prop email y le asignamos el valor
+            email: evt.target.value
+        });
+    }
+
+    onChangeEdad(evt){
+
+        this.setState({// como parámetro recibimos un objeto con la prop email y le asignamos el valor
+            edad: evt.target.value
+        });
+    }
+
+    onChangePassword(evt){
+
+        this.setState({
+            password: evt.target.value
+        });
+    }
+
+    onSubmit(evt){
+        evt.preventDefault();// Invocamos al servicio Http ajax fetch....
+        console.log(`Datos: ${this.state.nombre}, ${this.state.apellido}, ${this.state.password},${this.state.email},${this.state.edad}`)
+        window.fetch('http://localhost:4000/api/malt/registro', {
+            method: 'POST',
+            body: JSON.stringify({
+                "nombre": this.state.nombre,
+                "apellido": this.state.apellido,
+                "email": this.state.email,
+                "edad": this.state.edad,
+                "password":this.state.password
+            }), 
+            headers: {'Content-Type': 'application/json'}
+        }).then((res)=> alert('Usuario creado'))
+        .catch((vacas)=> 'Pues habra ido mal jojo')
+    }
+    
+
+    render(){
         return(
     
     <div className='contenedorRegistro wrapper'>
-    <div class="registrate col-xs-12">
+    <div className="registrate col-xs-12">
     <h1 className='unete col-xs-12'>Únete a maleteo y disfruta de sus ventajas</h1>
-    <div class="botonesFaceyGoogle col-xs-12">
-        <button class='facebookbutton'>Facebook</button>
-        <button class='googlebutton'> Google</button>
+    <div className="botonesFaceyGoogle col-xs-12">
+        <button className='facebookbutton'>Facebook</button>
+        <button className='googlebutton'> Google</button>
     </div>
     <p>o utiliza tu correo electrónico</p>
 
-    <div class="formularioRegistro col-xs-12">
-    <form class='formRegistro'action="">
-        <label for=""> Direccion de correo electrónico </label>
-        <input type="text" value="email" placeholder="Email"/>
+    <div className="formularioRegistro col-xs-12">
+    <form className='formRegistro'onSubmit={this.onSubmit}>
+        <label > Direccion de correo electrónico </label>
+        <input type="email" value={this.state.email} onChange={this.onChangeEmail} placeholder="Email"/>
         
-        <label for="">Nombre</label>
-        <input type="text" value="nombre" placeholder="Email"/>
+        <label >Nombre</label>
+        <input type="text" value={this.state.nombre} onChange={this.onChangeNombre} placeholder="Nombre"/>
         
-        <label for="">Apellido</label>
-        <input type="text" value="apellido" placeholder="Email"/>
+        <label >Apellido</label>
+        <input type="text" value={this.state.apellido} onChange={this.onChangeApellido}placeholder="Apellido"/>
         
-        <label for="">Contraseña</label>
-        <input type="password" value="password" placeholder="Email"/>
+        <label >Contraseña</label>
+        <input type="password" value={this.state.password} onChange={this.onChangePassword} placeholder="Password"/>
         
-        <label for="">Edad</label>
-        <input type="number" value="number" placeholder="Debes ser mayor de edad para registrarte"/>
+        <label >Edad</label>
+        <input type="number" value={this.state.edad} onChange={this.onChangeEdad} placeholder="Debes ser mayor de edad para registrarte" required/>
         <div className='checkboxRegistro'>
-        <input type="checkbox" value="checkbox" placeholder="Email"/>
-        <label for="checkbox">Quiero recibir consejos sobre como gestionar mi equipaje,ofertas y novedades.</label>
+        <input type="checkbox" value="checkbox" placeholder="Email" required/>
+        <label >Quiero recibir consejos sobre como gestionar mi equipaje,ofertas y novedades.</label>
         
         </div>
-        <button type="submit" class="botonRegistro">Resgístrate</button>
+        <button type="submit" className="botonRegistro" value="registrar">Resgístrate</button>
 
     </form>
     </div>
-</div>
-</div>
+    </div>
+    </div>
 
-) }}
+    ) }
+
+}
 
 export default RegistroUsuarios
