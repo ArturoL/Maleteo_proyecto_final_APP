2024e60b3bd6ad5228ffedb18a0d5421a1d3e533
Arturo.l <arturolanz.cre@gmail.com>
2020-01-29 17:59:28 +0100

    Registro/Login funcionales

--- app-front/src/componentes/LoginUsuario.js
+++ app-front/src/componentes/LoginUsuario.js
@@ -5,6 +5,55 @@ import './LoginUsuario.css';
 class LoginUsuario extends /*React.*/ Component{
 
 
+    constructor(props){
+        super(props);
+        this.state={
+            email: '',
+            password:'',
+        }
+        
+        this.onChangeEmail = this.onChangeEmail.bind(this);
+        this.onChangePassword = this.onChangePassword.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+
+    }
+
+    onChangeEmail(evt){
+
+        this.setState({
+            email: evt.target.value
+        });
+    }
+    onChangePassword(evt){
+
+        this.setState({
+            password: evt.target.value
+        });
+    }
+
+    onSubmit(evt){
+        evt.preventDefault();// Invocamos al servicio Http ajax fetch....
+        console.log(`Datos: ${this.state.password},${this.state.email}`)
+        window.fetch('http://localhost:4000/api/malt/login', {
+            method: 'POST',
+            body: JSON.stringify({
+                "email": this.state.email,
+                "password":this.state.password
+            }), 
+            headers: {'Content-Type': 'application/json'}
+        }).then((res)=> {
+            if (res.status === 200) {
+
+                window.location = '/home.html'
+                alert('Usuario login')
+           
+            } else {
+                alert('Usuario NO Autoriz')
+           
+            }})
+        .catch((vacas)=> 'Pues habra ido mal jojo')
+    }
+
 
 
 
@@ -26,12 +75,12 @@ class LoginUsuario extends /*React.*/ Component{
             <p>o utiliza tu correo electrónico</p>
         
             <div class="formularioLogin">
-            <form class='formLogin'action="">
+            <form class='formLogin' onSubmit={this.onSubmit}>
                 <label for="">Direccion de correo electrónico</label>
-                <input type="text" value="email" placeholder="Email"/>
+                <input type="text" value={this.state.email} onChange={this.onChangeEmail} placeholder="Email"/>
                 
                 <label for="">Contraseña</label>
-                <input type="password" value="password" placeholder="Contraseña"/>
+                <input type="password" value={this.state.password} onChange={this.onChangePassword} placeholder="Contraseña"/>
                 
                 <button type="submit" class="botonLogin">Iniciar sesión</button>
         
